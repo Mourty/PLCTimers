535709ef18bc0896e80972b1ab317f2a10b778f6
Mourt <90718225+Mourty@users.noreply.github.com>
2025-02-21 07:54:14 -0600

    Update TOF.ino
    fixed incorrect comment on code behavior.

--- examples/TOF/TOF.ino
+++ examples/TOF/TOF.ino
@@ -3,8 +3,8 @@ This example showcases how a Timer Off Delay (TOF) works. This type of timer is 
 compared to TON. The Done (DN) bit is set when the accumulated (ACC) does not equal the preset (PRE) amount.
 When the enable (EN) bit is set the Timer Timing (TT) bit will be set, and the accumulated value ACC will start counting.
 When the ACC = the preset (PRE) value, the DN bit will turn off.
-In this example we are using the built in LED, and a button that connects pin 3 to ground when pressed. the LED is always on
-unless the button is pressed for 1 second or more. The LED then turns off until the button is released.
+In this example we are using the built in LED, and a button that connects pin 3 to ground when pressed. the LED is always off
+unless the button is pressed. The LED will Turn on while the button is pressed. The LED will remain on for 1 second after the button is released.
 
 */
 #include <PLCTimer.h>
